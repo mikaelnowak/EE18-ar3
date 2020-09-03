5574845012b23b9d039d1e1d599538f8ce4ee9f6
Mikael Nowak <mikael.nowak@learnet.se>
2020-09-03 19:00:12 +0200

    starta med PHP

---
+++ kap1/taemot.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        echo $_REQUEST["anamn"];
+        echo "<br>";
+        echo $_REQUEST["pass"];
+        echo "<br>";
+        echo $_REQUEST["roll"];
+        echo "<br>";
+        echo $_REQUEST["an"];
+    ?>
+</body>
+</html>
