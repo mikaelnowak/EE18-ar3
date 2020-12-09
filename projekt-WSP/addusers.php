d383f1ce565bc25cedeb589bc9259257507b0683
Mikael Nowak <mikael.nowak@elev.ga.ntig.se>
2020-12-09 20:35:39 +0100

    Klar med user.php och snart klar med addusers.php

--- projekt-WSP/addusers.php
+++ projekt-WSP/addusers.php
@@ -9,13 +9,60 @@
 </head>
 <body>
     <div class="container">
-        <h1>Add chatroom</h1>
-        <form action="#" method="POST">
-            <label>User name</label>
-            <input type="text">
-            <label>User name</label>
-            <input type="text">
-        </form>
+        <div class="content small">
+            <h1 class="max-con">Add User</h1>
+            <form action="#" method="POST">
+                <label>User name:</label>
+                <input type="text" name="user-name" required>
+                <p class="description">No 2 users with the same name. Ex no two "Gumpe".</p>
+                <label>User color:</label>
+                <input type="text" name="user-color" required>
+                <p class="description">Write the color of choice's hex code counterpart "Ex: #f0f or #F0ecB12"</p>
+                <button type="submit">Add user</button>
+            </form>
+            <?php
+                //Ta ut det som finns i "User name" och "User color" inputsem
+                $userName = filter_input(INPUT_POST, 'user-name', FILTER_SANITIZE_STRING);
+                $userColor = filter_input(INPUT_POST, 'user-color', FILTER_SANITIZE_STRING);
+
+                //Variablar
+                $errorMessage = "";
+
+                if ($userName && $userColor) {
+                    //Kontrolera om namnet e unik
+                    $userNamesInDB = $conn->query('SELECT name FROM users');
+                    //var_dump($userNamesInDB);
+                    foreach ($userNamesInDB as $name) {
+                        if ($name[name] == $userName) {
+                            $userName = "";
+                            $errorMessage += "<p>There is allready a user named $userName, please get another username and try again</p>";
+                        }
+                    }
+
+                    //Kolla om man skriver in en färg namn eller hexkod för färg
+                    if (!preg_match('/^#([a-f0-9]{6}|[a-f0-9]{3})\b$/i', $userColor)) {
+                        $userColor = "";
+                        $errorMessage += "<p>The text you typed in dont follow the hex color rule, it a combination of red, green and blue color values. Search for an explenation and come back to type the correct color hex code.</p>";
+                    }
+
+                    //Om allt stämmer
+                    if ($userName != "" && $userColor != "") {
+                        $newUser = "INSERT INTO users (name, color) VALUES ('$userName', '$userColor')";
+                        $check = $conn->query($newUser);
+                        if (!$check) {
+                            echo "<p>There was an error when creating the user. Please come back later efter the bug is fixed.</p>
+                            <p>Error message: $conn->connect_error</p>";
+                        } else {
+                            echo "<div class=\"success\">
+                                    <p>Succes, your user with the username $userName and with its color have ben created, go back to the previous web page to access the user or other users.</p>
+                                </div>";
+                        }
+                    } else {
+                        echo "hej";
+                    }
+                }
+            ?>
+        </div>
     </div>
 </body>
 </html>
--- projekt-WSP/user.php
+++ projekt-WSP/user.php
@@ -9,26 +9,38 @@
 </head>
 <body>
     <div class="container">
-        <div class="select">
+        <div class="content">
             <h1 class="max-con">SELECT USER</h1>
             <div class="users">
             <?php
+            //Vissa alla användare i en grid
             $users = $conn->query('SELECT * FROM users');
             //var_dump($users);
-            foreach ($users as $key => $user) {
-                echo    "<a href=\"chatroom.php\">
-                            <div class=\"user\" style=\"border:5px solid $user[color];\" >
-                                <h2 class=\"min-con\">$user[name]</h2>
-                            </div>
-                        </a>";
+            foreach ($users as $user) {
+                echo "<a href=\"chatroom.php?id=$user[id]\">
+                        <div class=\"user\"style=\"
+                        border:5px solid $user[color];
+                        box-shadow: 0 0 5px $user[color];\">
+                            <h2 class=\"max-con\">$user[name]</h2>
+                        </div>
+                    </a>";
             }
             ?>
             </div>
-            <a href="addusers.php">
-                <div class="addusers">
-                    <h2>ADD USERS</h2>
-                </div>
-            </a>
+            <hr>
+            <h1 class="max-con">MANAGE</h1>
+            <div class="manage">
+                <a href="addusers.php">
+                    <div class="manage-part">
+                        <h2 class="max-con">ADD USERS</h2>
+                    </div>
+                </a>
+                <a href="remove.php">
+                    <div class="manage-part">
+                        <h2 class="max-con">MANAGE USERS</h2>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </body>
