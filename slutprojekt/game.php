3d15fa9842341df88156ba2b3488daae5cfab85d
mikaelnowak <54439937+mikaelnowak@users.noreply.github.com>
2021-05-28 21:46:53 +0200

    utdjgc

--- slutprojekt/game.php
+++ slutprojekt/game.php
@@ -12,10 +12,24 @@ if (!isset($_SESSION['user'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <div class="hej">
+    <header>
+        <ul class="nav">
+            <li class="nav-item">
+                <h1>Sites name</h1>
+            </li>
+            <li class="nav-item">
+                <img src="./pictures/6db57ae216790490f53cbd9e2a49d486.png" alt="Site logo">
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+        </ul>
+    </header>
+    <div class="game-container">
 
     </div>
 </body>
